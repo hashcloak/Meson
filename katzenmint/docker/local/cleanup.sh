5dea9608728b078262e5d4d940fadc7d2ec12fe1
sc0vu <alk03073135@gmail.com>
2022-01-23 04:24:55 -0800

    katzenmint: update cleanup.sh for local testnet

--- katzenmint/docker/local/cleanup.sh
+++ katzenmint/docker/local/cleanup.sh
@@ -1,44 +1,61 @@
 # /bin/sh
 
 echo "Clean up data generated when execute scripts..."
-rm -rf conf/node1/kdata/*
-rm -rf conf/node1/data/*
-rm -rf conf/node1/katzenmint
-echo "{
-  \"height\": \"0\",
-  \"round\": 0,
-  \"step\": 0
-}" > conf/node1/data/priv_validator_state.json
 
-rm -rf conf/node2/kdata/*
-rm -rf conf/node2/data/*
-rm -rf conf/node2/katzenmint
-echo "{
-  \"height\": \"0\",
-  \"round\": 0,
-  \"step\": 0
-}" > conf/node2/data/priv_validator_state.json
-
-rm -rf conf/node3/kdata/*
-rm -rf conf/node3/data/*
-rm -rf conf/node3/katzenmint
-echo "{
-  \"height\": \"0\",
-  \"round\": 0,
-  \"step\": 0
-}" > conf/node3/data/priv_validator_state.json
-
-rm -rf conf/mix1/data/*
-rm -rf conf/mix2/data/*
-rm -rf conf/mix3/data/*
-rm -rf conf/provider1/data/*
-rm -rf conf/provider2/data/*
-
-# Update genesis block time
 NOW=$(TZ=UTC date +"%Y-%m-%dT%H:%M:%S.000000Z")
 TITLE="\"genesis_time\""
-perl -i -pe"s/$TITLE.*/$TITLE: \"$NOW\",/g" conf/node1/config/genesis.json
-perl -i -pe"s/$TITLE.*/$TITLE: \"$NOW\",/g" conf/node2/config/genesis.json
-perl -i -pe"s/$TITLE.*/$TITLE: \"$NOW\",/g" conf/node3/config/genesis.json
+
+clean_mix_dir () {
+    d=$1
+    if [ -d $d ]; then
+        echo "Clean mix data: $d"
+        if [ -d $d/data ]; then
+            rm -rf $d/data/*
+        fi;
+    fi;
+}
+
+clean_provider_dir () {
+    d=$1
+    if [ -d $d ]; then
+        echo "Clean provider data: $d"
+        if [ -d $d/data ]; then
+            rm -rf $d/data/*
+        fi;
+    fi;
+}
+
+clean_katzenmint_dir () {
+    d=$1
+    if [ -d $d ]; then
+        echo "Clean katzenmint data: $d"
+        if [ ! -d $d/data ]; then
+            mkdir $d/data
+        fi;
+        if [ -d $d/data ]; then
+            rm -rf $d/data/*
+        fi;
+        if [ -d $d/kdata ]; then
+            rm -rf $d/kdata/*
+        fi;
+        if [ -d $d/katzenmint ]; then
+            rm -rf $d/katzenmint
+        fi;
+        echo "{
+          \"height\": \"0\",
+          \"round\": 0,
+          \"step\": 0
+        }" > $d/data/priv_validator_state.json
+        # Update genesis block time
+        perl -i -pe"s/$TITLE.*/$TITLE: \"$NOW\",/g" $d/config/genesis.json
+    fi;
+}
+
+for d in conf/* ; do
+    [ -L "${d%/}" ] && continue
+    [[ $d =~ ^conf/mix[1-3] ]] && clean_mix_dir $d
+    [[ $d =~ ^conf/provider[1-3] ]] && clean_provider_dir $d
+    [[ $d =~ ^conf/node[1-3] ]] && clean_katzenmint_dir $d
+done
 
 echo "Cleaned up!"
