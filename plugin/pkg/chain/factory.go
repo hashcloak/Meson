2ec9b25b0a1e2c4a038f624060908fc9dfb73471
sc0vu <alk03073135@gmail.com>
2023-10-18 13:53:26 +0800

    plugin: add kot testnet back

--- plugin/pkg/chain/factory.go
+++ plugin/pkg/chain/factory.go
@@ -15,6 +15,8 @@ func GetChain(ticker string) (IChain, error) {
 		return &ETHChain{ticker: "ETC", chainID: 61}, nil
 	case "GOR":
 		return &ETHChain{ticker: "GOR", chainID: 5}, nil
+	case "KOT":
+		return &ETHChain{ticker: "KOT", chainID: 6}, nil
 	case "SEP":
 		return &ETHChain{ticker: "SEP", chainID: 11155111}, nil
 	case "HOL":
